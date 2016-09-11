a1b6e7100483807d4bcd0b068d5d74212b779ded
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-09-11 23:03:28 +0000

    refactor: Pass source text to Compiler to keep single responsibility principle

--- lib/rubynception.rb
+++ lib/rubynception.rb
@@ -2,10 +2,11 @@ require 'pp'
 require File.expand_path(File.dirname(__FILE__) + '/rubynception/version')
 require File.expand_path(File.dirname(__FILE__) + '/rubynception/machine')
 
+# https://github.com/ruby/ruby/blob/trunk/insns.def
+
 module Rubynception
   class Compiler
-    def initialize(file)
-      source = File.read(file)
+    def initialize(source)
       magic, major_version, minor_version, format_type, misc,
       name, filename, filepath, line_no, type, locals, args,
       catch_table, bytecode = RubyVM::InstructionSequence.compile(source).to_a
