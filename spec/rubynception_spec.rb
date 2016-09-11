a173ad92e74558d54e9e02e830682af0325a2b15
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-09-11 23:04:52 +0000

    test: Test code execution of asset files

--- spec/rubynception_spec.rb
+++ spec/rubynception_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
-describe Rubynception do
-  it 'has a version number' do
-    expect(Rubynception::VERSION).not_to be nil
-  end
+def load_asset(name)
+  File.read(File.expand_path(File.dirname(__FILE__) + "/assets/#{name}.rb"))
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe Rubynception do
+  it 'performs ifs statements' do
+    source = load_asset('if')
+    expect { Rubynception::Compiler.new(source) }.to output(1).to_stdout 
   end
 end
